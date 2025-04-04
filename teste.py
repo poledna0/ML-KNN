8cc4288e9190bca6113fed295b4312e959199af1
poledna0 <henriquepoledna@proton.me>
2025-04-04 14:50:14 -0300

    cmnt

--- teste.py
+++ teste.py
@@ -8,7 +8,7 @@ data = pd.read_csv("iris.data")
 # renomeando as colunas
 data.columns = ["comp_sepoaneo", "argura_sepala", "comp_petala", "larg_petala", "especie"]
 
-#axis=0 - axis=1 - refere as linhas do DataFrame.
+#axis=0 - axis=1 - refere as linhas do dat
 
 x = data.drop("especie", axis=1)
 y = data["especie"]
@@ -31,11 +31,12 @@ x_treino, x_prova, y_treino, y_prova = train_test_split(x, y, test_size=0.2, ran
 #print("treino:", x_treino.shape, y_treino.shape)
 #print("testar:", x_prova.shape, y_prova.shape)
 
+# cria o modelo
 modelo = KNeighborsClassifier(n_neighbors=3, weights='distance')
 
 # esta sendo treinado
 modelo.fit(x_treino, y_treino)
 
-
+# mostra a saida, metricas de avaliacao
 print(classification_report(y_prova,modelo.predict(x_prova)))
 print(confusion_matrix(y_prova, modelo.predict(x_prova)))
